f069ffc7183bcae2e909b14555b5911ce70a1124
the-sugar-tree <sugar_tree@outlook.kr>
2022-11-16 16:53:52 +0900

    NotNull annotations

--- InventoryShare-api/src/main/java/com/sugar_tree/inventoryshare/api/Inventory.java
+++ InventoryShare-api/src/main/java/com/sugar_tree/inventoryshare/api/Inventory.java
@@ -37,7 +37,7 @@ public interface Inventory {
      * @throws NoSuchFieldException if a field with the specified name is not found.
      * @throws IllegalAccessException if this Field object is enforcing Java language access control and the underlying field is inaccessible or final; or if this Field object has no write access.
      */
-    default void setField(Object obj, String name, Object value) throws NoSuchFieldException, IllegalAccessException {
+    default void setField(@NotNull Object obj, @NotNull String name, Object value) throws NoSuchFieldException, IllegalAccessException {
         Field field = obj.getClass().getDeclaredField(name);
         field.setAccessible(true);
         field.set(obj, value);
