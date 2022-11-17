744ef071be439efbfd1d93ccc941688b987e8dfa
the-sugar-tree <sugar_tree@outlook.kr>
2022-11-17 11:47:46 +0900

    라이센스 업데이트

--- v1_16_R2/src/main/java/com/sugar_tree/inventoryshare/v1_16_R2/Inventory_1_16_R2.java
+++ v1_16_R2/src/main/java/com/sugar_tree/inventoryshare/v1_16_R2/Inventory_1_16_R2.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2021 the-sugar-tree
+ *
+ *  Licensed under the General Public License, Version 3.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/lgpl-3.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.sugar_tree.inventoryshare.v1_16_R2;
 
 import com.google.common.collect.ImmutableList;
