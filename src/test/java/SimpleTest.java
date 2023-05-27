0d4718e5242881aa58c600d4a69cd1a5c8ef755a
sugar_tree <sugar_tree@outlook.kr>
2023-05-28 00:06:22 +0900

    Added Sl4fj lib to test classes

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,3 +1,4 @@
+import lombok.extern.slf4j.Slf4j;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
@@ -7,21 +8,54 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.Locale;
 
+@Slf4j
 public class SimpleTest {
 
-    public static void main(String... args) throws IOException {
+    public static void main(String... args) throws IOException, InterruptedException {
         long startTime = System.currentTimeMillis();
+        
+        /*
+        Properties properties = System.getProperties();
+        properties.setProperty("user.language", "ko");
+        properties.setProperty("user.country", "kr");
+        System.setProperties(properties);
+        System.getProperties().forEach((o1, o2) -> {
+            System.out.println(o1+" : "+o2);
+        });
+        */
 
-        File file = new File("InventoryShare-core/src/main/resources/languages/lang_ko_kr.yml");
+//        checkLanguageConfig();
+        log.info("test");
+
+        new Task().start1().join();
+        log.info("{}s elapsed", ((float) (System.currentTimeMillis() - startTime) / 1000));
+    }
+
+    private static void checkLanguageConfig() throws IOException {
+        File file = new File("InventoryShare-core/src/main/resources/languages/lang_" + Locale.getDefault().toString().toLowerCase() + ".yml");
         if (!file.exists()) {
             throw new FileNotFoundException("File " + file.getAbsolutePath() + " does not exist.");
         }
-
         FileConfiguration fileConfiguration = YamlConfiguration.loadConfiguration(new InputStreamReader(Files.newInputStream(file.toPath()), StandardCharsets.UTF_8));
-
         fileConfiguration.getKeys(false).forEach((s) -> System.out.println(s + " : \"" + fileConfiguration.get(s) + "\""));
+    }
 
-        System.out.printf("%.4f%n", ((float) (System.currentTimeMillis() - startTime) / 1000));
+    private static class Task extends Thread {
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.info(Thread.currentThread().getName() + ": Hello");
+        }
+
+        public Task start1() {
+            super.start();
+            return this;
+        }
     }
 }
