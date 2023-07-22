81fe9110280323f6e1a4dc621c942ac37be06180
sugar_tree <sugar_tree@outlook.kr>
2023-07-22 22:08:19 +0900

    sponge dev test

---
+++ InventoryShare-sponge/src/main/java/com/sugar_tree/inventoryshare/InventoryShare.java
@@ -0,0 +1,19 @@
+package com.sugar_tree.inventoryshare;
+
+import com.google.inject.Inject;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.api.Server;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.lifecycle.StartedEngineEvent;
+import org.spongepowered.plugin.builtin.jvm.Plugin;
+
+@Plugin("inventoryshare")
+public class InventoryShare {
+    @Inject
+    private Logger logger;
+
+    @Listener
+    public void onServerStart(final StartedEngineEvent<Server> event) {
+        logger.info("Successfully running Plugin!");
+    }
+}
