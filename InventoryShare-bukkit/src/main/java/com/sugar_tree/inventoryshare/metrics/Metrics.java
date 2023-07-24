e60dc30cc5f0c5a8c6f08658185dedd8e7b62de5
sugar_tree <sugar_tree@outlook.kr>
2023-07-24 14:43:02 +0900

    Licencing

--- InventoryShare-bukkit/src/main/java/com/sugar_tree/inventoryshare/metrics/Metrics.java
+++ InventoryShare-bukkit/src/main/java/com/sugar_tree/inventoryshare/metrics/Metrics.java
@@ -1,3 +1,23 @@
+/*
+ * This file is part of the-sugar-tree, licensed under the GPL-3.0 License.
+ *
+ * InventoryShare the minecraft plugin that enables sharing inventory with ohter players
+ * Copyright (c) 2023 the-sugar-tree
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // https://github.com/Bastian/bstats-metrics/blob/single-file/bukkit/Metrics.java
 // https://github.com/Bastian/bstats-metrics/blob/fe4f4b0e050116441f877f59d30f0ddf382346fc/bukkit/Metrics.java (Version 3.0.2 used)
 /*
