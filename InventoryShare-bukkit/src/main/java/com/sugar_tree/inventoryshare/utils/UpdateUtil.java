ca906a6ad454d72b3f4d760095c224db1f9d4e02
sugar_tree <sugar_tree@outlook.kr>
2023-10-06 00:06:00 +0900

    pref: 업데이트 확인 방식 변경

--- InventoryShare-bukkit/src/main/java/com/sugar_tree/inventoryshare/utils/UpdateUtil.java
+++ InventoryShare-bukkit/src/main/java/com/sugar_tree/inventoryshare/utils/UpdateUtil.java
@@ -20,7 +20,12 @@
 
 package com.sugar_tree.inventoryshare.utils;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.*;
 
 import static com.sugar_tree.inventoryshare.SharedConstants.logger;
@@ -32,44 +37,28 @@ public final class UpdateUtil {
     private static String version;
     public static void checkUpdate() {
         try {
-            URL url = new URL("https://github.com/the-sugar-tree/InventoryShare/releases/latest");
-            String urls = getFinalURL(url).toString();
-            version = urls.substring(urls.lastIndexOf('/') + 1);
-        } catch (MalformedURLException e) {
-            logger.severe(I18NUtil.get(false, false, "update_error", e.getMessage()));
-        }
+            String url = "https://api.github.com/repos/the-sugar-tree/InventoryShare/releases/latest";
+            HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+            if (con != null) {
+                con.setConnectTimeout(1000);
+                con.setRequestMethod("GET");
 
+                int resCode = con.getResponseCode();
+
+                if (resCode == HttpURLConnection.HTTP_OK) {
+                    JSONObject value = (JSONObject) new JSONParser().parse(new InputStreamReader(con.getInputStream()));
+                    version = (String) value.get("tag_name");
+                }
+                con.disconnect();
+            }
+        } catch (IOException | ParseException e) {
+            logger.severe(I18NUtil.get(false, false, "update_error", e.getMessage()));
+            return;
+        }
         if (!("v"+plugin.getDescription().getVersion()).equals(version)) {
             logger.warning(I18NUtil.get(false, false, "update_able", plugin.getDescription().getVersion(), version));
             logger.warning("https://github.com/the-sugar-tree/InventoryShare/releases/latest");
         }
     }
 
-    public static URL getFinalURL(URL url) {
-        try {
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setInstanceFollowRedirects(false);
-            con.setRequestProperty("User-Agent",
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36");
-            con.addRequestProperty("Accept-Language", "en-US,en;q=0.8");
-            con.connect();
-            // Header에서 Status Code를 뽑는다.
-            int resCode = con.getResponseCode();
-            // http코드가 301(영구이동), 302(임시 이동), 303(기타 위치 보기) 이면 또다시 이 함수를 태운다. 재귀함수.
-            if (resCode == HttpURLConnection.HTTP_SEE_OTHER || resCode == HttpURLConnection.HTTP_MOVED_PERM
-                    || resCode == HttpURLConnection.HTTP_MOVED_TEMP) {
-                String Location = con.getHeaderField("Location");
-                if (Location.startsWith("/")) {
-                    Location = url.getProtocol() + "://" + url.getHost() + Location;
-                }
-                return getFinalURL(new URL(Location));
-            }
-        } catch (UnknownHostException | SocketTimeoutException | ConnectException e) {
-            logger.severe(I18NUtil.get("update_error", e.getMessage()));
-            System.out.println(e.getMessage());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return url;
-    }
 }
